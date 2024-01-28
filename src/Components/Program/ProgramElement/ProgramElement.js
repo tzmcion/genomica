43877592b87fb5b70c91958a8ca7f1c1de0c587b
tzmcion <tymsonekjelonek@gmail.com>
2024-01-28 14:45:09 +0100

    Changed program

--- src/Components/Program/ProgramElement/ProgramElement.js
+++ src/Components/Program/ProgramElement/ProgramElement.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ProgramElement.scss';
+
+function Event({el}){
+
+    const [open,setOpen] = useState(false);
+
+    const handleOpen = () =>{
+        setOpen(curr => !curr);
+    }
+
+    return (
+        <div onClick={handleOpen} className={`ProgramElement-Event ${open === true ? 'Event-Open' : ''}`}>
+                <div className='ProgramElement-Event-Title'>
+                    <img src={el.icon} alt='icon'/>
+                    <div className='ProgramElement-Event-Data'>
+                        <div className='ProgramElement-Event-Data-Left'>
+                            <h6>{el.time}</h6>
+                            <h5>{el.speaker}</h5>
+                        </div>
+                        <h4>{el.title}</h4>
+                    </div>
+                </div>
+                <div className='ProgramElement-Event-Description'>
+                    <p>{el.description}</p>
+                </div>
+        </div>
+    )
+}
 
 export default function ProgramElement({data,day}) {
 
     const renderElements = () =>{
         return data.map((el,index) =>{
-            return <div key={`Event${index}`} className='ProgramElement-Event'>
-                <img src={el.icon} alt='icon'/>
-                <div className='ProgramElement-Event-Data'>
-                    <h5>{el.speaker}</h5>
-                    <h4>{el.title}</h4>
-                </div>
-            </div>
+            return <Event key={`Event${index}`} el={el}/>
         })
     }
 
--- src/Utils/Data.js
+++ src/Utils/Data.js
@@ -1,4 +1,6 @@
 import null_icon from '../Assets/program/event.png';
+import main_event from '../Assets/program/main.png';
+import discussion from '../Assets/program/discussion.png';
 
 const Program = [
     {
@@ -6,16 +8,39 @@ const Program = [
         events:[
             {
                 title:"Crispr-Cas9 and fast cloning, What should we do with it?",
-                description:"Lorem",
+/*Max 410 Ch*/  description:"Proin vel sollicitudin justo, tempus viverra sem. Nullam sed libero vel dui imperdiet eleifend. Duis sed fringilla dolor. Fusce tempor enim diam. Sed non luctus leo, congue dignissim tellus. Suspendisse a massa sapien. Pellentesque interdum erat eget dui lobortis efficitur. Aenean ut diam lobortis, dapibus massa at, ornare nisi. Cras ullamcorper arcu sed porttitor dictum. Morbi tellus nisl, tristique in in.",    //Description Limited, Max 255*2 characters
+                time:"16:00, aula 103",
                 speaker:"Ivona Prestova",
+                icon:null_icon
+            },
+            {
+                title:"Discussion of the event with Pizza 🍕",
+                description:"Lorem",
+                time:"16:30, IZZIS Hall",
+                speaker:"All Attendets",
+                icon:discussion
+            },
+            {
+                title:"Was p53 just a fad, or is it still a future of cancer-preventing field of science",
+                description:"Lorem",
+                time:"17:00, Aula 103A & 103B",
+                speaker:"Maksymilian Gmur",
                 icon:null_icon
             },
             {
                 title:"How is Alternative splicing affected after RNA-viral infections of SARS-COV viral group",
                 description:"Lorem",
-                speaker:"Katarzyna Iwanowicz",
+                time:"18:00, aula 103",
+                speaker:"Katarzyna Iwanowicz-Sosenko",
+                icon:main_event
+            },
+            {
+                title:"[PL] Jak głodówka i stres może wpływać na zmiany w genomie płodu",
+                description:"Lorem",
+                time:"19:30, IZZIS Hall",
+                speaker:"Stanisław Wąsek",
                 icon:null_icon
-            }
+            },
         ]
     },
     {
@@ -24,6 +49,7 @@ const Program = [
             {
                 title:"Presentation genetics",
                 description:"Lorem",
+                time:"16:00, aula 103",
                 speaker:"Ivona Kovalicka",
                 icon:null_icon
             }
@@ -35,6 +61,7 @@ const Program = [
             {
                 title:"Presentation genetics",
                 description:"Lorem",
+                time:"16:00, aula 103",
                 speaker:"Shaquile O'neal",
                 icon:null_icon
             }
