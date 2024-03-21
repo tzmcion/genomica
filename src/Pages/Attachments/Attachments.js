71bac4d499762cb7b5c4fb380628505b1aa8f5e5
Kamil955 <kamil.kozik@microsoft.wsei.edu.pl>
2024-03-21 19:59:37 +0100

    Add abstract guides and styling to Attachments page

--- src/Pages/Attachments/Attachments.js
+++ src/Pages/Attachments/Attachments.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 export default function Attachments() {
     return (
-        <div>
+        <div style={{width: '1080px', margin: '10px auto'}}>
+            <h1>Abstracts</h1>
+            <p></p>
+            <p>Below you can find links for the abstracts examples.</p>
+            <p>Send them to our e-mail address <b>genomica.uj@gmail.com</b></p>
+
             <a href="https://drive.google.com/file/d/1lRZEhzLxxXjSO9998yAdFRQax8GanX6P/view?usp=sharing"> Abstract guide [PL]</a>
             <p></p>
+            <a href="https://drive.google.com/file/d/1vTzGFE_JzTVs3cQcaV_1UadgdGm6y-bK/view?usp=sharing"> Abstract guide [EN]</a>
         </div>
     )
 }
