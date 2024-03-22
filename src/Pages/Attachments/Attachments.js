3ade5a150528c21122aa763861f86abc90cb4494
Kamil955 <kamil.kozik@microsoft.wsei.edu.pl>
2024-03-22 17:57:27 +0100

    Update Attachments page with new declarations and abstract templates

--- src/Pages/Attachments/Attachments.js
+++ src/Pages/Attachments/Attachments.js
@@ -3,14 +3,18 @@ import React from 'react'
 export default function Attachments() {
     return (
         <div style={{width: '1080px', margin: '10px auto'}}>
-            <h1>Abstracts</h1>
+            <h1>Declarations</h1>
             <p></p>
-            <p>Below you can find links for the abstracts examples.</p>
-            <p>Send them to our e-mail address <b>genomica.uj@gmail.com</b></p>
+            <p>Below you can find links for the declarations examples. </p>
+            <p>Send them to our e-mail address with your abstracts<b>genomica.uj@gmail.com</b></p>
 
-            <a href="https://drive.google.com/file/d/1lRZEhzLxxXjSO9998yAdFRQax8GanX6P/view?usp=sharing"> Abstract guide [PL]</a>
+            <a href="https://drive.google.com/file/d/1l2M2ijuATs29zk0ppKyU-NyqmcLdRd3z/view?usp=sharing"> Declaration template [PL]</a>
             <p></p>
-            <a href="https://drive.google.com/file/d/1vTzGFE_JzTVs3cQcaV_1UadgdGm6y-bK/view?usp=sharing"> Abstract guide [EN]</a>
+            <a href="https://drive.google.com/file/d/1vTzGFE_JzTVs3cQcaV_1UadgdGm6y-bK/view?usp=sharing"> Declaration template [EN]</a>
+
+            <h1>Abstract template</h1>
+            <a href="https://drive.google.com/file/d/1lRZEhzLxxXjSO9998yAdFRQax8GanX6P/view?usp=sharing>" >Abstract template [PL]</a>
+
         </div>
     )
 }
