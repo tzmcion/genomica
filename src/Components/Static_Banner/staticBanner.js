acf906787e7ad3b362a212a0c7dbb871d37bebd7
tymsonekjelonek@gmail.com <tymsonekjelonek@gmail.com>
2025-03-31 14:57:07 +0100

    spelling...

--- src/Components/Static_Banner/staticBanner.js
+++ src/Components/Static_Banner/staticBanner.js
@@ -9,12 +9,12 @@ export default function staticBanner() {
         <div className='Static-Banner-Left'>
           <div className='Static-Banner-Left-Data'>
             <h2>As the New Year unfolds, we announce the</h2>
-            <h1>8th edition of the Genetics Conference "Genomica"</h1>
+            <h1>9th edition of the Genetics Conference "Genomica"</h1>
             <p><span className='delete_res'>The whole team is actively working on preparing the event, and we can now confirm its dates. Join us from May 23-25, 2025, at the Institute of Zoology, Jagiellonian University in Krakow.
               Genomica Conference is an excellent opportunity to broaden your horizons, networking, and experience active participation in the science conference.
               We encourage you to follow our social media for more information.</span></p>
             <p>The "Genomica" Student Conference of Genetics is an annual event gathering students, specialists, and outstanding scientists interested in genetics. It provides a unique opportunity to broaden your horizons, networking, and experience active participation in the science conference.
-              From May 23th to 25th this year, during the 8th edition of the "Genomica” Student Conference of Genetics, you will have the chance to share your passion, discuss fascinating research projects, and inspire each other. This event not only enhances knowledge but also creates a space for building a community of young scientists.
+              From May 23th to 25th this year, during the 9th edition of the "Genomica” Student Conference of Genetics, you will have the chance to share your passion, discuss fascinating research projects, and inspire each other. This event not only enhances knowledge but also creates a space for building a community of young scientists.
               We invite you to participate in this unique event where science meets passion, and inspiration arises from engaging dialogues. Reserve your time for the May days of the 9th edition of the "Genomica" Conference!</p>
               <p>See you there! 👋</p>
           </div>
